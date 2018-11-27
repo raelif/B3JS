92d7d9f9a7815c3539c68da858c1c2923dc47483
MATTEO <matteo.crielesi@gmail.com>
2018-11-27 16:37:39 +0100

    Application menu redesigned. Ready to add proper Blocks.

--- src/blockly_injection.js
+++ src/blockly_injection.js
@@ -1,78 +1,131 @@
 // /=====================================================================\
 //	Globals ~ all accessors
 // \=====================================================================/
-var webglArea = document.getElementById("webglArea");
-var webglCanvas = document.getElementById("webglCanvas");
+var webglArea = document.getElementById('webglArea');
+var webglCanvas = document.getElementById('webglCanvas');
 
-var blocklyArea = document.getElementById("blocklyArea");
-var blocklyDiv = document.getElementById("blocklyDiv");
+var blocklyArea = document.getElementById('blocklyArea');
+var blocklyDiv = document.getElementById('blocklyDiv');
 
-var xhr = new XMLHttpRequest();
+var checkExp = document.getElementById('checkExp');
+
 var workspace = undefined;
 
-// /=====================================================================\
-//	onload ~ onresize
-// \=====================================================================/
-xhr.onload = function () {
-	if (xhr.readyState === 4 && xhr.status === 200) {
+var onresize = function() {
+	// Resize blocklyDiv
+	blocklyDiv.style.width = blocklyArea.offsetWidth + "px";
+	blocklyDiv.style.height = blocklyArea.offsetHeight + "px";
+	Blockly.svgResize(workspace);
 
-		var options = {
-			toolbox : xhr.responseText,
-			collapse : true,
-			comments : true,
-			disable : true,
-			maxBlocks : Infinity,
-			trashcan : false,
-			horizontalLayout : false,
-			toolboxPosition : 'start',
-			css : true,
-			media : 'https://blockly-demo.appspot.com/static/media/',
-			rtl : false,
-			scrollbars : true,
-			sounds : true,
-			oneBasedIndex : true,
-			grid : {
-				spacing : 20,
-				length : 1,
-				colour : '#888',
-				snap : true
-			}
-		};
-
-		workspace = Blockly.inject(blocklyDiv, options);
-
-		var onresize = function(e) {
-			// Compute the absolute coordinates and dimensions of blocklyArea.
-			var element = blocklyArea;
-			var x = 0;
-			var y = 0;
-			do {
-				x += element.offsetLeft;
-				y += element.offsetTop;
-				element = element.offsetParent;
-			} while (element);
-
-			// Position blocklyDiv over blocklyArea.
-			blocklyDiv.style.left = x + "px";
-			blocklyDiv.style.top = y + "px";
-			blocklyDiv.style.width = blocklyArea.offsetWidth + "px";
-			blocklyDiv.style.height = blocklyArea.offsetHeight + "px";
-			Blockly.svgResize(workspace);
-
-			// Resize webglCanvas.
-			webglCanvas.style.width = webglArea.offsetWidth + "px";
-			webglCanvas.style.height = webglArea.offsetHeight + "px";
-		};
-
-		window.addEventListener("resize", onresize, false);
-		onresize();
-		Blockly.svgResize(workspace);
-	}
+	// Resize webglCanvas.
+	webglCanvas.style.width = webglArea.offsetWidth + "px";
+	webglCanvas.style.height = webglArea.offsetHeight + "px";
 };
 
-xhr.overrideMimeType("text/xml");
-xhr.open("GET", "src/toolbox.xml", true);
-xhr.send();
+window.addEventListener('resize', onresize, false);
+
+// /=====================================================================\
+//	void loadWorkspace()
+// \=====================================================================/
+function loadWorkspace() {
+	var xhr = new XMLHttpRequest();
+
+	xhr.onload = function() {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+
+			var options = {
+				toolbox : xhr.responseText,
+				collapse : true,
+				comments : true,
+				disable : true,
+				maxBlocks : Infinity,
+				trashcan : false,
+				horizontalLayout : false,
+				toolboxPosition : 'start',
+				css : true,
+				media : 'https://blockly-demo.appspot.com/static/media/',
+				rtl : false,
+				scrollbars : true,
+				sounds : true,
+				oneBasedIndex : true,
+				grid : {
+					spacing : 20,
+					length : 1,
+					colour : '#888',
+					snap : true
+				},
+				zoom : {
+					controls : false,
+					wheel : true,
+					startScale : 1,
+					maxScale : 3,
+					minScale : 0.3,
+					scaleSpeed : 1.2
+				}
+			};
+
+			workspace = Blockly.inject(blocklyDiv, options);
+			onresize();
+		}
+	};
+
+	xhr.overrideMimeType('text/xml');
+	if (checkExp.checked) {
+		xhr.open('GET', "src/exp_toolbox.xml", true);
+	} else {
+		xhr.open('GET', "src/toolbox.xml", true);
+	}
+	xhr.send();
+}
+
+// /=====================================================================\
+//	void saveProject()
+// \=====================================================================/
+function saveProject() {
+	// Generate JavaScript and Xml and save them.
+	Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+	var code = Blockly.JavaScript.workspaceToCode(workspace);
+	var xml_text = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
+
+	var filename = prompt("Save As");
+	console.log("Saved " + filename);
+
+	var downloadFile = function(content, type) {
+		var download = document.createElement('a');
+		download.style.display = 'none';
+		download.setAttribute('href', "data:text/" + type + "; charset=utf-8,"
+			+ encodeURIComponent(content));
+		type === 'xml' ? type = type : type = 'js';
+		download.setAttribute('download', filename + "." + type);
+
+		document.body.appendChild(download);
+		download.click();
+		document.body.removeChild(download);
+	};
+
+	if (filename !== null && filename !== "") {
+		downloadFile(code, 'javascript');
+		downloadFile(xml_text, 'xml');
+	}
+}
+
+// /=====================================================================\
+//	void importProject()
+// \=====================================================================/
+function importProject() {
+	var importedXml = document.getElementById('importedXml').files[0];
+	if (importedXml !== null && importedXml.type === 'text/xml') {
+		const fileReader = new FileReader();
+		fileReader.onload = function(e) {
+			var textFromFile = e.target.result;
+
+			workspace.clear();
+			Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(textFromFile), workspace);
+		};
+
+		fileReader.readAsText(importedXml, 'UTF-8');
+	}
+}
 
 // /=====================================================================\
 //	void openFullScreen()
@@ -90,52 +143,22 @@ function openFullscreen() {
 }
 
 // /=====================================================================\
-//	void saveProject()
+//	void expertMode()
 // \=====================================================================/
-function saveProject() {
-	// Generate JavaScript code and display it.
-	Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
-	var code = Blockly.JavaScript.workspaceToCode(workspace);
-	var xml_text = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
-
-	var filename = prompt("Save As");
-	console.log(filename);
-
-	var downloadFile = function(content, type) {
-		var download = document.createElement("a");
-		download.style.display = "none";
-		download.setAttribute("href", "data:text/" + type + "; charset=utf-8,"
-			+ encodeURIComponent(content));
-		type === "xml" ? type = type : type = "js";
-		download.setAttribute("download", filename + "." + type);
-
-		document.body.appendChild(download);
-		download.click();
-		document.body.removeChild(download);
-	};
-
-	if (filename !== null && filename !== "") {
-		downloadFile(code, "javascript");
-		downloadFile(xml_text, "xml");
-	}
+function expertMode() {
+	checkExp.checked = !checkExp.checked;
+	workspace.dispose();
+	loadWorkspace();
 }
 
 // /=====================================================================\
-//	void importProject()
+//	void showCode()
 // \=====================================================================/
-function importProject() {
-	var importedXml = document.getElementById("importedXml").files[0];
-	if (importedXml !== null) {
-		const fileReader = new FileReader();
-		fileReader.onload = function(e) {
-			var textFromFile = e.target.result;
-
-			workspace.clear();
-			Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(textFromFile), workspace);
-		};
-
-		fileReader.readAsText(importedXml, "UTF-8");
-	}
+function showCode() {
+	// Generate JavaScript code and display it.
+	Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+	var code = Blockly.JavaScript.workspaceToCode(workspace);
+	alert(code);
 }
 
 // /=====================================================================\
---
+++ src/exp_blocks.js
@@ -0,0 +1,209 @@
+// /=====================================================================\
+//	MUTATORS
+// \=====================================================================/
+Blockly.Blocks['arg_create_with_container'] = {
+	init: function() {
+		this.setColour(50);
+		this.appendStatementInput('STACK');
+		this.setTooltip("");
+		this.contextMenu = false;
+	}
+};
+
+Blockly.Blocks['arg_create_with_item'] = {
+	init: function() {
+		this.setColour(50);
+		this.appendDummyInput()
+				.appendField("arg");
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip("");
+		this.contextMenu = false;
+	}
+};
+
+// /=====================================================================\
+//	MUTATOR FUNCTIONS
+// \=====================================================================/
+function argMutationToDom() {
+	var container = document.createElement('mutation');
+	container.setAttribute('args', this.itemCount_);
+	return container;
+}
+
+function argDomToMutation(xmlElement) {
+	this.itemCount_ = parseInt(xmlElement.getAttribute('args'), 10);
+	this.updateShape_();
+}
+
+function argDecompose(workspace) {
+	var containerBlock = workspace.newBlock('arg_create_with_container');
+	containerBlock.initSvg();
+	var connection = containerBlock.getInput('STACK').connection;
+	for (var i = 0; i < this.itemCount_; i++) {
+		var itemBlock = workspace.newBlock('arg_create_with_item');
+		itemBlock.initSvg();
+		connection.connect(itemBlock.previousConnection);
+		connection = itemBlock.nextConnection;
+	}
+	return containerBlock;
+}
+
+function argCompose(containerBlock) {
+	var itemBlock = containerBlock.getInputTargetBlock('STACK');
+	// Count number of inputs.
+	var connections = [];
+	while (itemBlock) {
+		connections.push(itemBlock.valueConnection_);
+		itemBlock = itemBlock.nextConnection &&
+				itemBlock.nextConnection.targetBlock();
+	}
+	// Disconnect any children that don't belong.
+	for (var i = 0; i < this.itemCount_; i++) {
+		var connection = this.getInput('ADD' + i).connection.targetConnection;
+		if (connection && connections.indexOf(connection) == -1) {
+			connection.disconnect();
+		}
+	}
+	this.itemCount_ = connections.length;
+	this.updateShape_();
+	// Reconnect any child blocks.
+	for (var i = 0; i < this.itemCount_; i++) {
+		Blockly.Mutator.reconnect(connections[i], this, 'ADD' + i);
+	}
+}
+
+function argUpdateShape() {
+	if (this.itemCount_ && this.getInput('EMPTY')) {
+		this.removeInput('EMPTY');
+	}
+	// Add new inputs.
+	for (var i = 0; i < this.itemCount_; i++) {
+		if (!this.getInput('ADD' + i)) {
+			var input = this.appendValueInput('ADD' + i).setCheck("Number");
+			if (i == 0) {
+				input.appendField("with args");
+			}
+		}
+	}
+	// Remove deleted inputs.
+	while (this.getInput('ADD' + i)) {
+		this.removeInput('ADD' + i);
+		i++;
+	}
+}
+
+// /=====================================================================\
+//	BLOCKS
+// \=====================================================================/
+Blockly.Blocks['eb3js_dummy'] = {
+	init: function() {
+		this.appendDummyInput()
+				.appendField("dummy");
+		this.setInputsInline(true);
+		this.setColour(50);
+		this.setTooltip("");
+		this.setHelpUrl("");
+	}
+};
+
+Blockly.Blocks['eb3js_scene'] = {
+	init: function() {
+		this.appendDummyInput()
+				.appendField("scene");
+		this.setOutput(true, "Scene");
+		this.setColour(50);
+		this.setMutator(new Blockly.Mutator(['arg_create_with_item']));
+		this.setInputsInline(true);
+		this.setTooltip("");
+		this.setHelpUrl("https://threejs.org/docs/index.html#api/en/scenes/Scene");
+	},
+	mutationToDom: argMutationToDom,
+	domToMutation: argDomToMutation,
+	decompose: argDecompose,
+	compose: argCompose,
+	updateShape_: argUpdateShape
+};
+
+Blockly.Blocks['eb3js_scene_add'] = {
+	init: function() {
+		this.appendValueInput("RECEIVER")
+				.setCheck(["Scene", "Camera", "Geometry"])
+				.appendField("add to")
+				.appendField(new Blockly.FieldDropdown([["Scene","SCENE"], ["Camera","CAMERA"], ["Geometry","GEOMETRY"]]), "NAME");
+		this.appendValueInput("ELEMENT")
+				.setCheck("String")
+				.appendField("element");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true, null);
+		this.setNextStatement(true, null);
+		this.setColour(50);
+		this.setTooltip("");
+		this.setHelpUrl("");
+	}
+};
+
+// /=====================================================================\
+//	BLOCKS_CODE
+// \=====================================================================/
+Blockly.JavaScript['eb3js_dummy'] = function(block) {
+	// TODO: Assemble JavaScript into code variable.
+	var code =
+	// Scene - Context
+		"var scene = new THREE.Scene();\n"+
+		"var context = webglCanvas.getContext( 'webgl2' );\n"+
+
+		// Camera ~ Can use outside Scope!!!
+		"var camera = new THREE.PerspectiveCamera( 75, webglCanvas.offsetWidth/webglCanvas.offsetHeight, 0.1, 1000 );\n"+
+
+		// Renderer
+		"var renderer = new THREE.WebGLRenderer( { canvas: webglCanvas, context: context } );\n"+
+		"renderer.setSize( webglCanvas.offsetWidth, webglCanvas.offsetHeight );\n"+
+
+		// Cube = Geometry + Material
+		"var geometry = new THREE.BoxGeometry( 1, 1, 1 );\n"+
+		"var material = new THREE.MeshBasicMaterial( { color: 0xB30033 } );\n"+
+		"var cube = new THREE.Mesh( geometry, material );\n"+
+		"scene.add( cube );\n"+
+
+		"camera.position.z = 5;\n"+
+
+		"var animate = function () {\n"+
+		"	requestAnimationFrame( animate );\n"+
+
+		"	cube.rotation.x += 0.01;\n"+
+		"	cube.rotation.y += 0.01;\n"+
+
+		"	renderer.render( scene, camera );\n"+
+		"};\n"+
+		"animate();\n";
+
+	// TODO: Change ORDER_NONE to the correct strength.
+	return code;
+};
+
+Blockly.JavaScript['eb3js_scene'] = function(block) {
+	// Retrieve args list
+	var args = [block.itemCount_];
+  for (var i = 0; i < block.itemCount_; i++) {
+    args[i] = Blockly.JavaScript.valueToCode(block, 'ADD' + i,
+        Blockly.JavaScript.ORDER_COMMA) || 'null';
+  }
+	// TODO: Assemble JavaScript into code variable.
+	var code = "new THREE.Scene()";
+	console.log(args);
+
+	// TODO: Change ORDER_NONE to the correct strength.
+	return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['eb3js_scene_add'] = function(block) {
+	var dropdown_name = block.getFieldValue('NAME');
+	var value_receiver = Blockly.JavaScript.valueToCode(block, 'RECEIVER', Blockly.JavaScript.ORDER_ATOMIC);
+	var value_element = Blockly.JavaScript.valueToCode(block, 'ELEMENT', Blockly.JavaScript.ORDER_ATOMIC);
+	// TODO: Assemble JavaScript into code variable.
+	var code = '...;\n';
+	return code;
+};
+
+//	=====================================================================
